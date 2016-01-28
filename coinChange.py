ee7dbee0f8b9fed180c084ee01ae84eb2534dc10
sakamosa <sakamosa@users.noreply.github.com>
2016-01-28 00:11:10 -1000

    Create coinChange.py
    Skeleton for functions

---
+++ coinChange.py
@@ -0,0 +1,25 @@
+"""
+Three Coin Change Functions
+Parameters:
+    An array of cpins values in ascending order, the first is = 1
+    An integer value for Change
+Return Value:
+    Results[ [array of coins], number of coins]
+"""
+#Slow Change
+def slowChange(values, amount):
+    results = [values, amount]
+    return results
+
+#Greedy
+def greedy(values, amount):
+    results = [values, amount]
+    return results
+
+#Dynamic
+def dynamic(values, amount):
+    results = [values, amount]
+    return results
+
+
+
