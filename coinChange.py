e5aba19d5a25da75faa5b7bf4cd64c4be89a1ead
buckinja <buckinja@onid.oregonstate.edu>
2016-01-31 10:49:12 -0800

    cleaning up files, pushing correct coinChange.py file

--- coinChange.py
+++ coinChange.py
@@ -8,61 +8,8 @@ Return Value:
 """
 #Slow Change
 def slowChange(values, amount):
-    #Get the size of the list of values
-    size = len(values)
-
-    #Initialize the set of coins used to get the amount
-    coins = [0] * size
-
-    #Initialize the number of coins used to a maximum number
-    #Since one of the values will always be 1, the maximum
-    #number will be "amount" (the max would be "amount" pennies)
-    used = amount
-
-    #Get the minimum number of coins to get "amount" of change    
-    newUsed = slowChangeHelper(values, coins, amount, used)
-
-    return [coins, newUsed]
-
-"""
-Function: slowChangeHelper
-Description: Recursively finds the minimum amount of coins to get a
-             certain amount of change
-Parameters: vals: an array of coin values in ascending order, first is = 1
-            coinArr: An array of number of coins of each value used
-            amt: The integer value for the change
-            numCoins: The minimum number of coins used
-"""
-def slowChangeHelper(vals, coinArr, amt, numCoins):
-    #Get the size of the list of values
-    length = len(vals)
-
-    #Base Case: If there is an "amount"-cent coin, then that one coin is the min
-    for i in range(0, length):
-        if vals[i] == amt:
-            coinArr[i] = 1
-            numCoins = 1
-            return numCoins
-
-    # Recursive Case: for all i < "amount"
-    for i in range(1,amt):
-        #find the min # of coins needed for i-cents
-        coinsForI = [0] * length
-        minI = slowChangeHelper(vals, coinsForI, i, numCoins)
-        #find the min # of coins needed for "amount"-i cents
-        coinsForAmtMinusI = [0] * length
-        minAmtMinusI = slowChangeHelper(vals, coinsForAmtMinusI, amt-i, numCoins)
-
-        #If this is fewer than the previous best/min number of coins:
-        if minI + minAmtMinusI <= numCoins:
-            #Update the minimum number of coins
-            numCoins = minI + minAmtMinusI
-            #Update the array of coins used
-            for j in range(0,length):
-                coinArr[j] = coinsForI[j] + coinsForAmtMinusI[j]
-
-    #Return the minimum number of coins
-    return numCoins
+    results = [values, amount]
+    return results
 
 #Greedy
 def greedy(values, amount):
@@ -82,7 +29,7 @@ def greedy(values, amount):
 #Dynamic
 def dynamic(values, amount):
     size = len(values)
-    coins = values 
+    coins = values
     DPtable = [ [0] * size for i in range(amount + 1) ]
     DPtable[1][0] = 1
     
@@ -113,7 +60,7 @@ def dynamic(values, amount):
 
                 if (i + coins[j])  > amount:
                     break
-                elif (sums[i + coins[j]]) > (sums[i]+1):
+                elif (sums[i + coins[j]]) >= (sums[i]+1):
                     DPtable[i + coins[j]] = DPtable[i][:]
                     DPtable[i + coins[j]][j] += 1
                     sums[i + coins[j]] = sums[i] + 1
